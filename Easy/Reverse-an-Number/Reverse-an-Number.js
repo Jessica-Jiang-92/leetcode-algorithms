5e24d9eac14900228d076d4ffd7650da43aa414c
Jessica-Jiang-92 <82437559+Jessica-Jiang-92@users.noreply.github.com>
2021-05-27 16:17:30 +0800

    Update Reverse-an-Number.js

--- Easy/Reverse-an-Number/Reverse-an-Number.js
+++ Easy/Reverse-an-Number/Reverse-an-Number.js
@@ -1,1 +1,15 @@
+fun(num: number) {
+    let result = 0;
+    if (num > 2147483647 || num < -2147483647) {
+      result = 0;
+    } else {
+      const arr = num.toString().split('');
+      if (arr[0] === '-') {
+        result = -parseInt(arr.reverse().join(''));
+      } else {
+        result = parseInt(arr.reverse().join(''));
+      }
+    }
 
+    return result > 2147483647 || result < -2147483647 ? 0 : result;
+  }
